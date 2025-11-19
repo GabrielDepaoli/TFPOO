8c259efac3201b43d2ebc1ddfcc82ea4d359ec6e
GabrielDepaoli <depaoli.gab3@gmail.com>
2025-11-19 17:17:48 -0300

    dev: Construtores

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,12 @@
 package TFPOO;
 
 public class Cliente extends Pessoa {
+    private int rg;
+    private int telefone;
+
+    public Cliente(String nome, int rg, int telefone){
+        super (nome);
+        this.rg = rg;
+        this.telefone = telefone;
+    }
 }
--- Pessoa.java
+++ Pessoa.java
@@ -1,4 +1,14 @@
 package TFPOO;
 
 public abstract class Pessoa {
+    private String nome;
+    
+
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+
+    
+
+
 }
